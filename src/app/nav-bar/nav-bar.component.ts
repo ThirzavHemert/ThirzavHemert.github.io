f35f485415a88113e9b77c3b501453fc10c6c541
ThirzavHemert <thirzavanhemert@live.nl>
2024-11-20 09:15:25 +0100

    init

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    const listItems = this.elRef.nativeElement.querySelectorAll('ul li');
+
+    listItems.forEach((item: HTMLElement) => {
+      this.renderer.listen(item, 'click', () => {
+        listItems.forEach((el: HTMLElement) => this.renderer.removeClass(el, 'clicked'));
+        this.renderer.addClass(item, 'clicked');
+      });
+    });
+  }
 }
