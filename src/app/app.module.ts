a9f00158daefa76483334afede46cffd99d562f3
ThirzavHemert <thirzavanhemert@live.nl>
2024-11-20 08:46:03 +0100

    init landingpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './home/landing-page/landing-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
