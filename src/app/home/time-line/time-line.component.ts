43d37763d67c74430ef22bc57db4e98e68bcd419
Thirza <thirzahemert@gmail.com>
2025-03-13 20:10:04 +0100

    Timeline content framework

--- src/app/home/time-line/time-line.component.ts
+++ src/app/home/time-line/time-line.component.ts
@@ -1,9 +1,5 @@
 import { AfterViewInit, Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
-
-interface Project {
-  title: string;
-  year: number;
-}
+import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-time-line',
@@ -11,23 +7,23 @@ interface Project {
   styleUrls: ['./time-line.component.css']
 })
 export class TimeLineComponent implements OnInit {
-  isExpanded = false;
+  isExpanded = true;
 
   projects: Project[] = [
-    { title: 'Project A', year: 2021 },
-    { title: 'Project B', year: 2021 },
-    { title: 'Project C', year: 2021 },
-    { title: 'Project D', year: 2022 },
-    { title: 'Project E', year: 2022 },
-    { title: 'Project F', year: 2022 },
-    { title: 'Project G', year: 2022 },
-    { title: 'Project H', year: 2023 },
-    { title: 'Project I', year: 2023 },
-    { title: 'Project J', year: 2023 },
-    { title: 'Project K', year: 2023 },
+    { title: 'Project A', year: 2021, description: 'Description of Project A', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project B', year: 2021, description: 'Description of Project B', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project C', year: 2021, description: 'Description of Project C', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project D', year: 2022, description: 'Description of Project D', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project E', year: 2022, description: 'Description of Project E', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project F', year: 2022, description: 'Description of Project F', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project G', year: 2022, description: 'Description of Project G', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project H', year: 2023, description: 'Description of Project H', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project I', year: 2023, description: 'Description of Project I', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project J', year: 2023, description: 'Description of Project J', image: 'https://loremflickr.com/200/200?random=1' },
+    { title: 'Project K', year: 2023, description: 'Description of Project K', image: 'https://loremflickr.com/200/200?random=1' },
   ];
 
-  groupedProjects: { [key: number]: string[] } = {};
+  groupedProjects: { [year: number]: Project[] } = {};
 
   ngOnInit() {
     this.groupProjectsByYear();
@@ -42,22 +38,29 @@ export class TimeLineComponent implements OnInit {
   }
 
   groupProjectsByYear() {
-    const projectMap: { [year: number]: string[] } = {};
-
+    const projectMap: { [year: number]: Project[] } = {}; // Store full objects
+  
     this.projects.forEach((project) => {
       if (!projectMap[project.year]) {
         projectMap[project.year] = [];
       }
-      projectMap[project.year].push(project.title);
+      projectMap[project.year].push(project); // Store full project object
     });
-
+  
     // Ensure each year's project list has an odd number of items
     for (const year in projectMap) {
       if (projectMap[year].length % 2 === 0) {
-        projectMap[year].push('Placeholder'); // Add a filler item
+        projectMap[year].push({
+          title: "Placeholder",
+          year: parseInt(year),
+          description: "",
+          image: ""
+        }); // Add a full placeholder project
       }
     }
-
+  
     this.groupedProjects = projectMap;
+    console.log(this.groupedProjects); // Debugging
   }
+  
 }
