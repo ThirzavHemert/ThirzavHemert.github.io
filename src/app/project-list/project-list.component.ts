06fe1a9061178d91d91db0a7dd8b81579b637fb5
Thirza <thirzahemert@gmail.com>
2025-04-25 00:38:08 +0200

    Added design to projects list

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -12,43 +12,43 @@ export class ProjectListComponent {
       id: 0,
       title: 'Shiba Inu App',
       year: 2020,
-      description: 'A platform for Shiba lovers to share stories and photos. ',
-      image: 'https://picsum.photos/300/300?random=1'
+      description: 'A platform for Shiba lovers to share stories and photos. A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos. A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos.A platform for Shiba lovers to share stories and photos. ',
+      image: 'https://picsum.photos/300/400?random=1'
     },
     {
       id: 1,
       title: 'Mountain Explorer',
       year: 2020,
       description: 'Track your mountain hikes with this lightweight app.',
-      image: 'https://picsum.photos/300/300?random=2'
+      image: 'https://picsum.photos/300/400?random=2'
     },
     {
       id: 2,
       title: 'Recipe Finder',
       year: 2021,
       description: 'Find recipes with whatever you have in your fridge.',
-      image: 'https://picsum.photos/300/300?random=3'
+      image: 'https://picsum.photos/300/400?random=3'
     },
     {
       id: 3,
       title: 'Shiba Inu App',
       year: 2020,
       description: 'A platform for Shiba lovers to share stories and photos.',
-      image: 'https://picsum.photos/300/300?random=1'
+      image: 'https://picsum.photos/300/400?random=1'
     },
     {
       id: 4,
       title: 'Mountain Explorer',
       year: 2020,
       description: 'Track your mountain hikes with this lightweight app.',
-      image: 'https://picsum.photos/300/300?random=2'
+      image: 'https://picsum.photos/300/400?random=2'
     },
     {
       id: 5,
       title: 'Recipe Finder',
       year: 2021,
       description: 'Find recipes with whatever you have in your fridge.',
-      image: 'https://picsum.photos/300/300?random=3'
+      image: 'https://picsum.photos/300/400?random=3'
     }
   ];
 
