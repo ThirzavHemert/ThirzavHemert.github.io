2664d5a5a192a47b753c7208b60108a2b766f20b
Thirza <thirzahemert@gmail.com>
2025-04-24 15:13:03 +0200

    Its something

--- src/app/project-list/project-list-item/project-list-item.component.ts
+++ src/app/project-list/project-list-item/project-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from 'src/app/models/project.model';
 
 @Component({
   selector: 'app-project-list-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-list-item.component.css']
 })
 export class ProjectListItemComponent {
-
+  @Input() project!: Project;
 }
--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent {
+  //Temp mock data
+  projects = [
+    {
+      id: 0,
+      title: 'Shiba Inu App',
+      year: 2020,
+      description: 'A platform for Shiba lovers to share stories and photos.',
+      image: 'https://picsum.photos/600/300?random=1'
+    },
+    {
+      id: 1,
+      title: 'Mountain Explorer',
+      year: 2020,
+      description: 'Track your mountain hikes with this lightweight app.',
+      image: 'https://picsum.photos/600/300?random=2'
+    },
+    {
+      id: 2,
+      title: 'Recipe Finder',
+      year: 2021,
+      description: 'Find recipes with whatever you have in your fridge.',
+      image: 'https://picsum.photos/600/300?random=3'
+    }
+  ];
 
 }
